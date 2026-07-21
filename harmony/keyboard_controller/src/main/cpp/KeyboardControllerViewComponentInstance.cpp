6da8af2a737930bcad99f8e262f40552b0499a5f
sky603629 <ding_chengjie@hoperun.com>
2026-07-21 17:29:11 +0800

    feat(harmony): 实现 onFocusedInputLayoutChanged 布局事件

--- harmony/keyboard_controller/src/main/cpp/KeyboardControllerViewComponentInstance.cpp
+++ harmony/keyboard_controller/src/main/cpp/KeyboardControllerViewComponentInstance.cpp
@@ -38,6 +38,7 @@ KeyboardControllerViewComponentInstance::KeyboardControllerViewComponentInstance
 
     m_customNode.setCustomNodeDelegate(this);
 }
+
 void KeyboardControllerViewComponentInstance::onChildInserted(ComponentInstance::Shared const &childComponentInstance,
                                                               std::size_t index) {
     CppComponentInstance::onChildInserted(childComponentInstance, index);
@@ -76,6 +77,9 @@ void KeyboardControllerViewComponentInstance::onPropsChanged(SharedConcreteProps
 void KeyboardControllerViewComponentInstance::onCommandReceived(std::string const &commandName,
                                                                 folly::dynamic const &args) {
     CppComponentInstance::onCommandReceived(commandName, args);
+    if (commandName == "synchronizeFocusedInputLayout") {
+        syncUpLayout();
+    }
 }
 
 ArkUINode &KeyboardControllerViewComponentInstance::getLocalRootArkUINode() { return m_customNode; }
@@ -98,6 +102,10 @@ void KeyboardControllerViewComponentInstance::onMessageReceived(ArkTSMessage con
         }
         if (height == 0) {
             this->keyboardStatus = KeyboardControllerStatus::HIDE;
+            this->keyboardHeight = 0;
+        }
+        if (this->enabled) {
+            syncUpLayout();
         }
         this->keyboardHeightChangeHandle();
     }
@@ -188,6 +196,7 @@ void KeyboardControllerViewComponentInstance::onChange(std::string text) {
     facebook::react::KeyboardControllerViewEventEmitter::TextChangeEvent event = {text};
     if(this->enabled){
         m_eventEmitter->onFocusedInputTextChanged(event);
+        syncUpLayout();
      }
 
 }
@@ -229,9 +238,19 @@ void KeyboardControllerViewComponentInstance::focusDidSet() {
 void KeyboardControllerViewComponentInstance::onFocus() {
     DLOG(INFO) << "onKeyboardControllerView onFocus";
     this->focusDidSet();
+    syncUpLayout();
 }
 
-void KeyboardControllerViewComponentInstance::onBlur() { DLOG(INFO) << " onKeyboardControllerView onBlur"; }
+void KeyboardControllerViewComponentInstance::onBlur() {
+    DLOG(INFO) << " onKeyboardControllerView onBlur";
+    m_lastLayoutEvent = FocusedInputLayoutData{};
+    if (m_eventEmitter && this->enabled) {
+        facebook::react::KeyboardControllerViewEventEmitter::InputLayoutEvent payload = {};
+        payload.target = -1;
+        payload.parentScrollViewTarget = -1;
+        m_eventEmitter->onFocusedInputLayoutChanged(payload);
+    }
+}
 
 /**
  * 设置焦点到指定方向的输入框
@@ -284,5 +303,94 @@ void KeyboardControllerViewComponentInstance::setFocusTo(const std::string& dire
         DLOG(INFO) << "setFocusTo: no target input found in direction " << direction;
     }
     this->textInputVector.clear();
+}
+
+TextInputComponentInstance::Shared KeyboardControllerViewComponentInstance::findFocusedTextInput() {
+    auto allInputs = ViewHierarchyNavigator::getAllInputFields(this->shared_from_this());
+    for (auto &input : allInputs) {
+        ArkUINode &node = input->getLocalRootArkUINode();
+        if (node.isFocused()) {
+            return input;
+        }
+    }
+    return nullptr;
+}
+
+int KeyboardControllerViewComponentInstance::findParentScrollViewTarget(ComponentInstance::Shared const &input) {
+    auto parent = input->getParent().lock();
+    while (parent != nullptr) {
+        std::string name = parent->getComponentName();
+        if (name.find("ScrollView") != std::string::npos) {
+            return static_cast<int>(parent->getTag());
+        }
+        parent = parent->getParent().lock();
+    }
+    return -1;
+}
+
+double KeyboardControllerViewComponentInstance::pxToVp(double px) const {
+    auto pointScaleFactor = this->getLayoutMetrics().pointScaleFactor;
+    if (pointScaleFactor == 0.0) {
+        return px;
+    }
+    return px / pointScaleFactor;
+}
+
+void KeyboardControllerViewComponentInstance::syncUpLayout() {
+    auto focusedInput = findFocusedTextInput();
+    if (!focusedInput) {
+        return;
+    }
+
+    ArkUI_NodeHandle handle = focusedInput->getLocalRootArkUINode().getArkUINodeHandle();
+    if (handle == nullptr) {
+        return;
+    }
+
+    ArkUI_IntOffset windowOffset{};
+    ArkUI_IntOffset localOffset{};
+    ArkUI_IntSize size{};
+    int32_t windowRet = OH_ArkUI_NodeUtils_GetLayoutPositionInWindow(handle, &windowOffset);
+    int32_t localRet = OH_ArkUI_NodeUtils_GetLayoutPosition(handle, &localOffset);
+    int32_t sizeRet = OH_ArkUI_NodeUtils_GetLayoutSize(handle, &size);
+    if (windowRet != ARKUI_ERROR_CODE_NO_ERROR ||
+        localRet != ARKUI_ERROR_CODE_NO_ERROR ||
+        sizeRet != ARKUI_ERROR_CODE_NO_ERROR) {
+        DLOG(WARNING) << "syncUpLayout: measure failed windowRet=" << windowRet
+                      << " localRet=" << localRet << " sizeRet=" << sizeRet;
+        return;
+    }
+
+    FocusedInputLayoutData event;
+    event.absoluteX = pxToVp(static_cast<double>(windowOffset.x));
+    event.absoluteY = pxToVp(static_cast<double>(windowOffset.y));
+    event.x = pxToVp(static_cast<double>(localOffset.x));
+    event.y = pxToVp(static_cast<double>(localOffset.y));
+    event.width = pxToVp(static_cast<double>(size.width));
+    event.height = pxToVp(static_cast<double>(size.height));
+    event.target = static_cast<int>(focusedInput->getTag());
+    event.parentScrollViewTarget = findParentScrollViewTarget(focusedInput);
+
+    dispatchLayoutToJS(event);
+}
+
+void KeyboardControllerViewComponentInstance::dispatchLayoutToJS(FocusedInputLayoutData const &event) {
+    if (event == m_lastLayoutEvent) {
+        return;
+    }
+
+    m_lastLayoutEvent = event;
+    if (m_eventEmitter && this->enabled) {
+        facebook::react::KeyboardControllerViewEventEmitter::InputLayoutEvent payload = {};
+        payload.target = event.target;
+        payload.parentScrollViewTarget = event.parentScrollViewTarget;
+        payload.layout.absoluteX = event.absoluteX;
+        payload.layout.absoluteY = event.absoluteY;
+        payload.layout.x = event.x;
+        payload.layout.y = event.y;
+        payload.layout.width = event.width;
+        payload.layout.height = event.height;
+        m_eventEmitter->onFocusedInputLayoutChanged(payload);
+    }
 }
 } // namespace rnoh
